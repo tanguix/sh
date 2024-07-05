8fd0259192c980dc090c7de4432491986604bbf7
tanguix <mikiyax09@gmail.com>
2024-07-05 15:27:54 +0800

    smaller amount push

--- server/app/database.py
+++
@@ -1,7 +0,0 @@
-
-from pymongo import MongoClient
-
-# this file right now is only for connecting database
-client = MongoClient('localhost', 27017)
-db = client.flask_database  # use this temp
-
--- server/app/logger.py
+++
@@ -1,8 +0,0 @@
-
-# ---------------------------------- logging information ----------------------------------
-import logging
-
-# logging level: WARNING > DEBUGGING
-logging.basicConfig(level=logging.WARNING)  # suppress all log message below WARNING level
-logger = logging.getLogger(__name__)
-
