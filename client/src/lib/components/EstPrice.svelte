45be1977cf457d18af2e029886cd699507592265
tanguix <mikiyax09@gmail.com>
2024-08-09 17:59:27 +0800

    next: export all the data from calculators into something for review

--- client/src/lib/components/EstPrice.svelte
+++ client/src/lib/components/EstPrice.svelte
@@ -537,7 +537,7 @@
     border-radius: 8px;
     margin-bottom: 30px;
     padding: 20px;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    /* box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06); */
     background-color: #ffffff;
   }
 
